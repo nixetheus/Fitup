c2c4b713471b5495fd59d7ececef731abb35b2c0
nixetheus <luca.cassenti.00@gmail.com>
2023-07-30 22:44:31 +0200

    Exercise List Activity Clean

--- app/src/main/java/it/polimi/mobile/design/ExerciseListActivity.kt
+++ app/src/main/java/it/polimi/mobile/design/ExerciseListActivity.kt
@@ -1,8 +1,8 @@
 package it.polimi.mobile.design
 
 import android.annotation.SuppressLint
-import android.content.ContentValues
 import android.content.Intent
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -12,25 +12,22 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import it.polimi.mobile.design.databinding.ActivityExerciseListBinding
 import it.polimi.mobile.design.databinding.FragmentExerciseListBinding
 import it.polimi.mobile.design.databinding.FragmentFilterBinding
 import it.polimi.mobile.design.entities.Exercise
-import it.polimi.mobile.design.entities.Workout
 import it.polimi.mobile.design.enum.ExerciseType
 import it.polimi.mobile.design.helpers.DatabaseHelper
 import it.polimi.mobile.design.helpers.HelperFunctions
 
 class ExerciseListActivity : AppCompatActivity() {
 
+    private val helperDB = DatabaseHelper().getInstance()
     private lateinit var binding: ActivityExerciseListBinding
-    private var exerciseDatabase = FirebaseDatabase.getInstance().getReference("Exercise")
-    private val databaseHelperInstance = DatabaseHelper().getInstance()
 
-    private var exerciseArrayList = ArrayList<Exercise>()
     private var filters = arrayListOf<Int>()
+    private var exerciseArrayList = ArrayList<Exercise>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -40,19 +37,63 @@ class ExerciseListActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setBindings()
+        showFilters()
+        retrieveExercises()
+    }
 
-        exerciseDatabase.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                exerciseArrayList = databaseHelperInstance.getExercisesFromSnapshot(snapshot)
-                showExercises(exerciseArrayList)
-            }
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("Firebase", "Couldn't retrieve data...")
+    private fun setBindings() {
+
+        binding.homeButton.setOnClickListener{
+            val intent = Intent(this, CentralActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.addExerciseButton.setOnClickListener{
+            showAddExercise()
+        }
+
+        binding.addExerciseClose.setOnClickListener{
+            hideAddExercise()
+        }
+
+        binding.confirmAddExerciseBtn.setOnClickListener {
+            saveExercise()
+            hideAddExercise()
+        }
+
+        binding.closeExerciseMenu.setOnClickListener {
+            binding.exerciseMenuCard.visibility = View.GONE
+        }
+
+        binding.searchExercise.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?) = false
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                showExercises(exerciseArrayList.filter { exercise ->
+                    exercise.name?.contains(p0.orEmpty(), ignoreCase = true) == true ||
+                            exercise.type.toString().contains(p0.orEmpty(), ignoreCase = true)
+                })
+                return false
             }
         })
+    }
 
-        showFilters()
+    private fun showAddExercise() {
+        binding.addExerciseCard.visibility = View.VISIBLE
+        val openAddExerciseMenuAnimation = TranslateAnimation(
+            0F, 0F, binding.addExerciseCard.height.toFloat(), 0F)
+        openAddExerciseMenuAnimation.duration = 500
+        openAddExerciseMenuAnimation.fillAfter = true
+        binding.addExerciseCard.startAnimation(openAddExerciseMenuAnimation)
+    }
 
+    private fun hideAddExercise() {
+        binding.addExerciseCard.visibility = View.GONE
+        val closeAddExerciseMenuAnimation = TranslateAnimation(
+            0F, 0F, 0F, binding.addExerciseCard.height.toFloat() + 5.toPx())
+        closeAddExerciseMenuAnimation.duration = 500
+        closeAddExerciseMenuAnimation.fillAfter = true
+        binding.addExerciseCard.startAnimation(closeAddExerciseMenuAnimation)
     }
 
     @SuppressLint("Recycle")
@@ -60,24 +101,28 @@ class ExerciseListActivity : AppCompatActivity() {
 
         for (typeFilter in ExerciseType.values()) {
 
-            val filterLayout = FragmentFilterBinding.inflate(layoutInflater)
-            filterLayout.filterDisplayName.text = typeFilter.name.replace("_", " ").lowercase().split(' ')
-                .joinToString(" ") { it.replaceFirstChar(Char::uppercaseChar) }
-
+            val filterLayout = FragmentFilterBinding.inflate(layoutInflater).apply {
+                filterDisplayName.text = typeFilter.name.replace("_", " ").lowercase()
+                    .split(' ').joinToString(" ") { it.replaceFirstChar(Char::uppercaseChar) }
+            }
 
             val attrs = intArrayOf(R.attr.colorSecondary)
             val ta = obtainStyledAttributes(R.style.Theme_MobileProject, attrs)
 
             filterLayout.filterCard.setOnClickListener {
-                if (typeFilter.ordinal !in filters) {
-                    filters.add(typeFilter.ordinal)
-                    filterLayout.filterDisplayName.setBackgroundColor(resources.getColor(R.color.lilla, applicationContext.theme))
-                } else {
-                    filters.remove(typeFilter.ordinal)
-                    filterLayout.filterDisplayName.setBackgroundColor(ta.getColor(0, Color.BLACK))
+                filters.runCatching {
+                    if (typeFilter.ordinal !in this) {
+                        add(typeFilter.ordinal)
+                        filterLayout.filterDisplayName.setBackgroundColor(resources.getColor(R.color.lilla, applicationContext.theme))
+                    } else {
+                        remove(typeFilter.ordinal)
+                        filterLayout.filterDisplayName.setBackgroundColor(ta.getColor(0, Color.BLACK))
+                    }
+                    applyFilter()
                 }
-                applyFilter()
             }
+
+            binding.filtersLayout.addView(filterLayout.root)
         }
     }
 
@@ -86,73 +131,16 @@ class ExerciseListActivity : AppCompatActivity() {
         else showExercises(exerciseArrayList.filter { filters.contains(it.type!!.ordinal)})
     }
 
-    private fun setBindings() {
-
-        binding.confirmAddExerciseBtn.setOnClickListener{
-            createExercise()
-            binding.addExerciseCard.visibility = View.GONE
-        }
-        binding.addExerciseButton.setOnClickListener{
-            binding.addExerciseCard.visibility = View.VISIBLE
-        }
-        binding.addExerciseClose.setOnClickListener{
-            binding.addExerciseCard.visibility = View.GONE
-        }
-
-        binding.homeButton.setOnClickListener{
-            val intent = Intent(this, CentralActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.searchExercise.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(p0: String?): Boolean {
-                return false
+    private fun retrieveExercises() {
+        helperDB.exercisesSchema.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                exerciseArrayList = helperDB.getExercisesFromSnapshot(snapshot)
+                showExercises(exerciseArrayList)
             }
-            override fun onQueryTextChange(p0: String?): Boolean {
-                showExercises(exerciseArrayList.filter { exercise ->
-                    exercise.name!!.lowercase().contains(p0!!.lowercase())
-                            || exercise.type.toString().lowercase().contains(p0.lowercase())})
-                return false
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("Firebase", "Couldn't retrieve data...")
             }
         })
-
-        // Close workout menu
-        binding.closeExerciseMenu.setOnClickListener{
-            val animate = TranslateAnimation(0F, 0F, 0F, binding.exerciseMenuCard.height.toFloat())
-            animate.duration = 500
-            animate.fillAfter = true
-            binding.exerciseMenuCard.startAnimation(animate)
-        }
-    }
-
-    private fun createExercise(){
-
-        val exerciseName = binding.exerciseNameField.text.toString()
-        val kcalPerReps = HelperFunctions().parseFloatInput(binding.kcalInputValue.text.toString())
-        val exType = exerciseTypeFromString(binding.typeOfEx.selectedItem.toString())
-        val exp = HelperFunctions().parseFloatInput(binding.expInputValue.text.toString())
-
-        val exerciseId = exerciseDatabase.push().key!!
-        if (exerciseName.isNotEmpty()){
-            val exercise = Exercise(exerciseId, exerciseName, kcalPerReps, exType, exp)
-            exerciseDatabase.child(exerciseId).setValue(exercise).addOnSuccessListener {
-                Toast.makeText(this, "Successfully saved!!", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, ExerciseListActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }
-        else
-            Toast.makeText(this, "Fill in all fields to continue", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun exerciseTypeFromString(typeString : String) : ExerciseType {
-        return when(typeString) {
-            "Arms" -> ExerciseType.ARMS
-            "Legs" -> ExerciseType.LEGS
-            "Chest" -> ExerciseType.CHEST
-            else -> ExerciseType.ABDOMEN
-        }
     }
 
     private fun showExercises(exercises: List<Exercise>) {
@@ -161,66 +149,65 @@ class ExerciseListActivity : AppCompatActivity() {
         for (exercise in exercises.sortedWith(compareBy({ it.type }, { it.name!!.length }))) {
 
             val exerciseLayout = FragmentExerciseListBinding.inflate(layoutInflater)
-            exerciseLayout.exerciseNameList.text = exercise.name!!.replaceFirstChar { it.uppercaseChar() }
-            exerciseLayout.caloriesRepsValue.text = exercise.caloriesPerRep!!.toString()
-            exerciseLayout.experienceValue.text = exercise.experiencePerReps!!.toString()
+            with(exerciseLayout) {
+                exerciseNameList.text  = exercise.name!!.replaceFirstChar { it.uppercaseChar() }
+                caloriesRepsValue.text = exercise.caloriesPerRep!!.toString()
+                experienceValue.text   = exercise.experiencePerReps!!.toString()
 
-            exerciseLayout.exerciseCard.setOnLongClickListener {
-
-                //binding.exerciseMenuCard.visibility = View.VISIBLE
-                binding.exerciseMenuName.text = exercise.name
-                   // .replaceFirstChar { it.uppercaseChar() }
-                val animate = TranslateAnimation(0F, 0F, binding.addExerciseCard.height.toFloat(), 0F)
-                animate.duration = 500
-                animate.fillAfter = true
-                binding.deleteExerciseLayout.startAnimation(animate)
-
-                binding.deleteExerciseButton.setOnClickListener{
-                    onDeleteExercise(exercise)
+                exerciseCard.setOnLongClickListener {
+                    binding.exerciseMenuCard.visibility = View.VISIBLE
+                    binding.exerciseMenuName.text = exercise.name!!.uppercase()
+                    val animate = TranslateAnimation(0F, 0F, binding.addExerciseCard.height.toFloat(), 0F)
+                    animate.duration = 500
+                    animate.fillAfter = true
+                    binding.deleteExerciseLayout.startAnimation(animate)
+                    binding.deleteExerciseButton.setOnClickListener {
+                        onDeleteExercise(exercise)
+                    }
+                    false
                 }
-                false
             }
-
             binding.exercisesListLayout.addView(exerciseLayout.root)
         }
     }
 
-    private fun onDeleteExercise(exercise: Exercise) {
+    private fun saveExercise() {
 
-        /*binding.exerciseMenuCard.visibility = View.GONE
-
-        val exercise = database.reference.child("WorkoutExercise").orderByChild("workoutId")
-            .equalTo(exercise.eid)
-
-        exercise.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for (exerciseSnap in dataSnapshot.children) {
-                    exerciseSnap.ref.removeValue()
-                }
+        val exerciseId = helperDB.exercisesSchema.push().key!!
+        if (binding.exerciseNameField.text.toString().isNotEmpty()) {
+            val exercise = createExercise(exerciseId)
+            helperDB.exercisesSchema.child(exerciseId).setValue(exercise).addOnSuccessListener {
+                Toast.makeText(this, "Successfully saved!!", Toast.LENGTH_SHORT).show()
             }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.e(ContentValues.TAG, "onCancelled", databaseError.toException())
-            }
-        })
-
-
-        val workoutToRemove =
-            database.reference.child("Workout").orderByChild("workoutId").equalTo(workout.workoutId)
-
-        workoutToRemove.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                for (workoutSnapshot in dataSnapshot.children) {
-                    workoutSnapshot.ref.removeValue()
-                }
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-                Log.e(ContentValues.TAG, "onCancelled", databaseError.toException())
-            }
-        })
-
-        val intent = Intent(this, WorkoutListActivity::class.java)
-        startActivity(intent)*/
+        }
+        else
+            Toast.makeText(this, "Fill in all fields to continue", Toast.LENGTH_SHORT).show()
     }
+
+    private fun createExercise(exerciseId: String) : Exercise {
+
+        val exerciseName = binding.exerciseNameField.text.toString()
+        val kcalPerReps  = HelperFunctions().parseFloatInput(binding.kcalInputValue.text.toString())
+        val exType       = exerciseTypeFromString(binding.typeOfEx.selectedItem.toString())
+        val exp          = HelperFunctions().parseFloatInput(binding.expInputValue.text.toString())
+
+        return Exercise(exerciseId, exerciseName, kcalPerReps, exType, exp)
+    }
+
+    private fun exerciseTypeFromString(typeString : String) : ExerciseType {
+        return when(typeString) {
+            "ARMS" -> ExerciseType.ARMS
+            "LEGS" -> ExerciseType.LEGS
+            "CHEST" -> ExerciseType.CHEST
+            else -> ExerciseType.ABDOMEN
+        }
+    }
+
+    private fun onDeleteExercise(exercise: Exercise) {
+        binding.exerciseMenuCard.visibility = View.GONE
+        helperDB.exercisesSchema.child(exercise.eid!!).removeValue()
+        Toast.makeText(this, "Exercise Eliminated", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).toInt()
 }
